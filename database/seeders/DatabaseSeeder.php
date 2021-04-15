110ffda7f4922d6403f3f036ea836ac18b1806e4
Matthieu Mota <matthieumota@gmail.com>
2021-04-15 15:24:40 +0200

    Migration et seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        DB::table('properties')->insert([
+            'title' => 'Maison',
+            'description' => 'Une super maison',
+            'price' => rand(25000, 150000),
+            'sold' => rand(0, 1),
+            'user_id' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
