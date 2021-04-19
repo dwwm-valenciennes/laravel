c039e3ae9c6d108763c76574b24a5df30246ebe1
Matthieu Mota <matthieumota@gmail.com>
2021-04-19 14:54:42 +0200

    Property factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Property;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,12 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+        \App\Models\User::factory(5)->unverified()->create();
 
+        // 100 annonces totalement aléatoires et pas cohérentes
+        Property::factory(100)->create([
+            // On prend 1 des 10 utilisateurs précédent de manière
+            // aléatoire
+            'user_id' => User::all()->random()
+        ]);
+
+        // 5 annonces bien cohérentes
         $properties = ['Maison', 'Appartement', 'Garage', 'T3', 'Terrasse'];
 
         foreach ($properties as $property) {
-            DB::table('properties')->insert([
+            Property::factory()->create([
+                'title' => $property,
+                'user_id' => User::all()->random()
+            ]);
+            /*DB::table('properties')->insert([
                 'title' => $property,
                 'description' => 'Une super annonce pour '.$property,
                 'price' => rand(25000, 150000),
@@ -27,7 +42,7 @@ class DatabaseSeeder extends Seeder
                 'user_id' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
-            ]);
+            ]);*/
         }
     }
 }
